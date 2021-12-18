2694ff2f531e51baa5e02ce729f6371dce17cbde
Airi <adi73434@gmail.com>
2021-12-18 03:40:20 +0000

    Create floating-point.js

---
+++ js/floating-point.js
@@ -0,0 +1,16 @@
+const decimalA = 0.1;
+const decimalB = 0.2;
+const decimalC = decimalA + decimalB
+
+console.log("Decimal C is: " + decimalC);
+
+if (0.3 === decimalC) { console.log("Same") }
+
+
+const digitA = 1;
+const digitB = 2;
+const digitC = digitA + digitB
+
+console.log("Digit C is: " + digitC);
+
+if (0.3 === digitC) { console.log("Same") }
