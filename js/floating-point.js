2298dd95a10dc0f4151f86689e4a0f6ae829b880
airi <adi73434@gmail.com>
2023-08-24 19:06:07 +0100

    format

--- js/floating-point.js
+++ js/floating-point.js
@@ -1,16 +1,20 @@
 const decimalA = 0.1;
 const decimalB = 0.2;
-const decimalC = decimalA + decimalB
+const decimalC = decimalA + decimalB;
 
 console.log("Decimal C is: " + decimalC);
 
-if (0.3 === decimalC) { console.log("Same") }
+if (0.3 === decimalC) {
+	console.log("Same");
+}
 
 
 const digitA = 1;
 const digitB = 2;
-const digitC = digitA + digitB
+const digitC = digitA + digitB;
 
 console.log("Digit C is: " + digitC);
 
-if (3 === digitC) { console.log("Same") }
+if (3 === digitC) {
+	console.log("Same");
+}
